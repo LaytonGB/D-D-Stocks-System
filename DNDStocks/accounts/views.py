3c5441c8f0766848ba65185933ef35968528fc35
Layton <laytonb@hotmail.co.uk>
2021-03-31 08:15:56 +0100

    improve mypy config

--- DNDStocks/accounts/views.py
+++ DNDStocks/accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 
-def get_user(request) -> User or None:
+def get_user(request) -> User:
     user = request.user
     if isinstance(user, User):
         return user
